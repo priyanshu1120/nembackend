ab7bc9cee5cae59db07b0c87b3384c26b178809c
priyanshu1120 <priyanshukaushik796@gmail.com>
2022-12-11 17:52:58 +0530

    a1

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 app.use(express.json())
 const {connect} = require("./config/db")
 const cors = require("cors")
+const {userRouter} = require("./routes/user.route")
 require("dotenv").config()
 app.use(cors({
     origin:"*"
@@ -12,7 +13,7 @@ app.get("/",(req,res)=>{
     res.send("server run ")
 })
 
-// app.use("/users",userRouter)
+app.use("/users",userRouter)
 
 // app.use(authentication)
 // app.use("/notes",noteRouter)
