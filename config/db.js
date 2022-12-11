5c627962452e4d40043aaa9201b721a5710f93f1
priyanshu1120 <priyanshukaushik796@gmail.com>
2022-12-11 17:16:07 +0530

    run5

--- config/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose")
- require('dotenv').config()
-mongoose.set('strictQuery',false)
-const connect = mongoose.connect(process.env.MONGOURL)
-module.exports = {connect}
