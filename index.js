a42a319873eab9d81d90e93d7df8dba88136cf15
priyanshu1120 <priyanshukaushik796@gmail.com>
2022-12-11 16:42:27 +0530

    server add 4

--- index.js
+++ index.js
@@ -9,13 +9,11 @@ const {userRouter} = require("./routes/user.route")
 const {noteRouter} =  require("./routes/note.route")
 const {authentication} = require("./middleware/authentication")
 
-app.use("/users",userRouter)
-
-
-
 app.get("/",(req,res)=>{
     res.send("server run ")
 })
+
+app.use("/users",userRouter)
 
 // app.use(authentication)
 app.use("/notes",noteRouter)
